f78d0a9c614e8597de42e463c510a76ae1516517
konakona666 <konakona666@proton.me>
2024-12-22 08:42:02 +0500

    add quant fp8 interface

--- q8_kernels/functional/quantizer.py
+++ q8_kernels/functional/quantizer.py
@@ -6,9 +6,13 @@ from typing import Tuple
 
 class TokenWiseQuantizer(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x) -> torch.Tensor:
-        return tokenwise_quant(x)
+    def forward(ctx, x, out_type) -> Tuple[torch.Tensor, torch.Tensor]:
+        return tokenwise_quant(x, out_type)
 
 
 def quantize(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-    return TokenWiseQuantizer.apply(x)
+    return TokenWiseQuantizer.apply(x, None)
+
+def quantize_fp8(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    return TokenWiseQuantizer.apply(x, torch.float8_e4m3fn)
+
